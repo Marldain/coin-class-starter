4a87e84a20682e4735d49723ce7b6e67d4884e86
Justin Moore <marldainlunas@yahoo.com>
2020-11-19 22:05:32 -0500

    Initital Commit

---
+++ assets/scripts/coin.js
@@ -0,0 +1,19 @@
+class Coin {
+  constructor(state) {
+    this.state = state;
+  }
+
+  flip() {
+    console.log("coin.flip() has been invoked");
+  }
+
+  toString() {
+    console.log("coin.toString() has been invoked");
+  }
+
+  toHTML() {
+    let img = document.createElement("img");
+    console.log("coin.toHTML() has been invoked");
+    return img;
+  }
+}
